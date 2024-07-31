89f734f044b7a4bef790214508411c060b716dc7
CharlesHiggins-stai <charles.higgins@kcl.ac.uk>
2024-07-31 18:55:18 +0000

    writing document to find solution

--- experiments/lrp_wrapper.py
+++ experiments/lrp_wrapper.py
@@ -97,10 +97,10 @@ class WrapperNet(nn.Module):
         with track_activations(self):
             y =  self.model(x)
         if target_class != None:
-            relevance = y.gather(1, target_class.unsqueeze(1))
-            # mask = torch.zeros_like(y)
-            # mask.scatter_(1, target_class.unsqueeze(1), 1)
-            # relevance = y * mask
+            # relevance = y.gather(1, target_class.unsqueeze(1))
+            mask = torch.zeros_like(y)
+            mask.scatter_(1, target_class.unsqueeze(1), 1)
+            relevance = y * mask
         else:
             # relevance = F.log_softmax(y) 
             relevance = diff_softmax(y)
--- internal_utils/get_data.py
+++ internal_utils/get_data.py
@@ -4,7 +4,7 @@ from torchvision.transforms import Normalize
 from torchvision import datasets
 import sys
 ROOT_DIR = "/home/charleshiggins/RL-LRP"
-def get_CIFAR10_dataloader(train:bool = True):
+def get_CIFAR10_dataloader(train:bool = True, batch_size = 64, num_workers = 0, pin_memory = False):
 
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                         std=[0.229, 0.224, 0.225])
@@ -12,6 +12,6 @@ def get_CIFAR10_dataloader(train:bool = True):
             datasets.CIFAR10(root=f'{ROOT_DIR}/baselines/trainVggBaselineForCIFAR10/data', train=train, download=False, transform=transforms.Compose([
                 transforms.ToTensor()
             ])),
-            batch_size=64, shuffle=False,
-            num_workers=0, pin_memory=False)
+            batch_size=batch_size, shuffle=False,
+            num_workers=num_workers, pin_memory=pin_memory)
     return loader
